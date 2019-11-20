6637aa975af98f337c7d578c47548f8666548a62
Yeonggwan Cho <yk03171@naver.com>
2019-11-20 20:06:39 +0900

    [add] project init

---
+++ src/main/java/com/example/re_project/ReProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.re_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/re_project/ReProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.re_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
